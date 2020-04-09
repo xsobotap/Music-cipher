239944e3036368b4f99482c6e0b1f06ed8082732
xsobotap <mirosobota7@gmail.com>
2020-04-09 11:23:43 +0200

    First incomplete applications of AES

---
+++ src/main/java/AESCypher.java
@@ -0,0 +1,124 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class AESCypher {
+
+    public static SecretKey generateSecretKey(String password, byte [] iv) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), iv, 65536, 128); // AES-128
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] key = secretKeyFactory.generateSecret(spec).getEncoded();
+        return new SecretKeySpec(key, "AES");
+    }
+
+    public byte[] Encrypt(String key, byte[] data) throws InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        //Prepare the nonce
+        SecureRandom secureRandom = new SecureRandom();
+
+        //Noonce should be 12 bytes
+        byte[] iv = new byte[12];
+        secureRandom.nextBytes(iv);
+
+        SecretKey secretKey = generateSecretKey(key, iv);
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
+        //Encryption mode on!
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
+        //Encrypt the data
+        byte [] encryptedData = cipher.doFinal(data);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4 + iv.length + encryptedData.length);
+        byteBuffer.putInt(iv.length);
+        byteBuffer.put(iv);
+        byteBuffer.put(encryptedData);
+        return byteBuffer.array();
+
+    }
+
+    public static byte [] Decrypt(String key, byte [] encryptedData)
+            throws NoSuchPaddingException,
+            NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException,
+            InvalidKeyException,
+            BadPaddingException,
+            IllegalBlockSizeException,
+            InvalidKeySpecException {
+
+
+        //Wrap the data into a byte buffer to ease the reading process
+        ByteBuffer byteBuffer = ByteBuffer.wrap(encryptedData);
+
+        int noonceSize = byteBuffer.getInt();
+
+        //Make sure that the file was encrypted properly
+        if(noonceSize < 12 || noonceSize >= 16) {
+            throw new IllegalArgumentException("Nonce size is incorrect. Make sure that the incoming data is an AES encrypted file.");
+        }
+        byte[] iv = new byte[noonceSize];
+        byteBuffer.get(iv);
+
+        //Prepare your key/password
+        SecretKey secretKey = generateSecretKey(key, iv);
+
+        //get the rest of encrypted data
+        byte[] cipherBytes = new byte[byteBuffer.remaining()];
+        byteBuffer.get(cipherBytes);
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
+
+        //Encryption mode on!
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+
+        //Encrypt the data
+        return cipher.doFinal(cipherBytes);
+
+    }
+
+
+   // private static final String ALGO = "AES";
+    //private byte[] keyValue;
+
+/*    public AESCypher(String key){
+        keyValue=key.getBytes();
+    }
+
+    public String encrypt(String Data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Key key = this.generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE,key);
+        //byte[] encodedValue = c.doFinal(Data.getBytes());
+
+        byte[] encodedValue= new BigInteger(Data, 2).toByteArray();
+
+        String encryptedValue = new BASE64Encoder().encode(encodedValue);
+        return encryptedValue;
+
+    }
+
+    public String decrypt(String encryptedData) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE,key);
+        byte[] decoderValue = new BASE64Decoder().decodeBuffer(encryptedData);
+        byte[] decValue = c.doFinal(decoderValue);
+        String decryptedValue = new String(decValue);
+        return decryptedValue;
+
+    }
+
+    private Key generateKey(){
+        return new SecretKeySpec(keyValue,ALGO);
+    }*/
+}
